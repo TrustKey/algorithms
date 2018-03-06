621bc76fede30268d3c10581f820de23ae5c9ce6
Igor Nikolaychuk <jmpeax@icloud.com>
2018-03-06 23:28:59 +0200

    test implementation

---
+++ errorCodes.js
@@ -0,0 +1,8 @@
+module.exports = {
+    1: "Request validation error",
+    2: "Wrong or missing seed",
+    3: "Unable to resolve promise",
+    4: "Bad min/max params",
+    5: "Short seed",
+    6: "Unable to generate sub promise",
+};
